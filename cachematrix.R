acb797db0cc784dd98c3a6f08e2a08ba3914a56f
Eric Lu <pplyfpp@gmail.com>
2021-07-26 02:00:02 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#Cache the given matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(matrix) {
+                mtx <<- matrix
+                inv <<- NULL
+        }
+        get <- function() x 
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
+
+#Test codes
+matrix1 = matrix(c(1,1,1,2,3,4,3,4,4),3,3) #Creating a non-singular matrix
+a <- makeCacheMatrix(matrix1) 
+b <- cacheSolve(a)
+a$get() %*% a$getinverse() #It should returns a identity matrix
